8d1f8ee3c0725726fda89419069809f7bf9ce8a7
jungahzzzang <jungahzzzang@gmail.com>
2022-04-28 14:10:42 +0900

    이미지 업로드 완료

--- routes/page.js
+++ routes/page.js
@@ -1,37 +1,64 @@
 const express = require('express');
-//로그인 모듈 추가
-const passport = require('passport');
-const bcrypt = require('bcrypt');
-const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
-const User = require('../models/user');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User, Hashtag } = require('../models');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
-	res.locals.user = null;
-	res.locals.followerCount = 0;
-	res.locals.followingCount = 0;
-	res.locals.followerIdList = [];
-	next();
+  res.locals.user = req.user;
+  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+  res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+  res.locals.followerIdList = req.user ? req.user.Followings.map(f => f.id) : [];
+  next();
 });
 
-//자신의 프로필은 로그인을 해야 볼 수 있으므로 isLoggedIn 미들웨어 사용
-router.get('/profile',isLoggedIn, (req,res)=>{
-	res.render('profile',{title:'내 정보 = NodeBird'});
+router.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird' });
 });
 
-//회원가입 페이지는 로그인하지 않은 사람에게만 보여야하므로 isNotLoggedIn 미들웨어 사용
-router.get('/join',isNotLoggedIn,(req,res)=>{
-	res.render('join',{title:'회원가입 - NodeBird'});
+router.get('/join', isNotLoggedIn, (req, res) => {
+  res.render('join', { title: '회원가입 - NodeBird' });
 });
 
-router.get('/', (req, res, next) => {
-	const twits = [];
-	res.render('main', {
-		title: 'NodeBird',
-		twits,
-	});
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nick'],
+      },
+      order: [['createdAt', 'DESC']],
+    });
+    res.render('main', {
+      title: 'NodeBird',
+      twits: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
+router.get('/hashtag', async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect('/');
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User }] });
+    }
+
+    return res.render('main', {
+      title: `${query} | NodeBird`,
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
 
 module.exports = router;
